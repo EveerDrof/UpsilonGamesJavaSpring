ce8b9f72c9a6c4e1d16fad4da22cab6273a7b8d1
Michael <hyper00border@gmail.com>
2022-01-24 21:23:02 +0300

    Minor format fix

--- src/main/java/com/diploma/UpsilonGames/votes/VoteRepository.java
+++ src/main/java/com/diploma/UpsilonGames/votes/VoteRepository.java
@@ -7,7 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface VoteRepository extends JpaRepository<Vote, Long> {
-    @Query(value = "SELECT COUNT(*)  FROM vote v WHERE v.review_id = ?1 AND v.vote = true",
+    @Query(value = "SELECT COUNT(*) " +
+            " FROM vote v WHERE v.review_id = ?1 AND v.vote = true",
             nativeQuery = true)
     long getReviewLikesNumber(Review reviewId);
 
