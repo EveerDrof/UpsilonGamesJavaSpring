ad64d2661a1f663dc0a6caa7c3649bb8eb37eac6
Michael <hyper00border@gmail.com>
2022-01-24 21:37:30 +0300

    minor format fix

--- src/main/java/com/diploma/UpsilonGames/storeRecords/StoreRecordRepository.java
+++ src/main/java/com/diploma/UpsilonGames/storeRecords/StoreRecordRepository.java
@@ -11,27 +11,32 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Repository
-public interface StoreRecordRepository extends JpaRepository<StoreRecord,Long> {
+public interface StoreRecordRepository extends JpaRepository<StoreRecord, Long> {
     @Query(value = "SELECT sr.game_id FROM store_record sr WHERE sr.user_id = ?1 " +
             " AND sr.store_record_type = ?2"
-            ,nativeQuery = true)
+            , nativeQuery = true)
     List<Long> getUserGamesWithType(User userId, String storeRecordType);
+
     @Query(value = "UPDATE store_record sr SET sr.store_record_type = ?3 " +
             " WHERE sr.user_id = ?1 AND sr.store_record_type = ?2"
-            ,nativeQuery = true)
+            , nativeQuery = true)
     @Modifying
     @Transactional
     void switchUserGamesType(User user, String oldRecordType, String newRecordType);
+
     @Query(value = "SELECT * FROM store_record sr WHERE sr.game_id = ?1 AND sr.user_id = ?2 "
-            ,nativeQuery = true)
+            , nativeQuery = true)
     Game findByGameIdAndUserId(Game game, User user);
+
     @Query(value = "SELECT EXISTS (SELECT * FROM store_record sr WHERE sr.game_id = ?1  AND" +
             " sr.user_id = ?2 AND sr.store_record_type = ?3)"
-            ,nativeQuery = true)
-    long existsByGameIdAndUserIdAndType(Game game, User user,String storeRecordType);
+            , nativeQuery = true)
+    long existsByGameIdAndUserIdAndType(Game game, User user, String storeRecordType);
+
     @Modifying
     @Transactional
-    @Query(value = "DELETE FROM store_record sr WHERE sr.game_id = ?1 AND sr.user_id = ?2",
+    @Query(value = "DELETE FROM store_record sr " +
+            " WHERE sr.game_id = ?1 AND sr.user_id = ?2",
             nativeQuery = true)
     void deleteByGameIdAndUserId(Game gameId, User userId);
 }
